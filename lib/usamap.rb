feb7671946c3bb8e3e4b636515dcfd9dee62f673
salemine technologies <info@salemine.com>
2013-04-11 13:45:02 -0600

    fix the module structure

--- lib/usamap.rb
+++ lib/usamap.rb
@@ -1,23 +1,22 @@
 require 'usa_counties_svg'
 
 # @author salemine <info@salemine.com>
-module USAmap
-  class << self
-    # @param [Hash] stats_h stats to plot by county FIPS value eg stats_h ['35001'] = 3
-    # @param [Hash] colors_h colors to plot for numeric ranges eg colors_h [0..5] = '#CD5C5C'
-    def initialize(stats_h = {}, colors_h = {})
-      @stats_h, @colors_h = stats_h, colors_h
-    end
+class USAmap
+  include UsaCountiesSvg
+  # @param [Hash] stats_h stats to plot by county FIPS value eg stats_h ['35001'] = 3
+  # @param [Hash] colors_h colors to plot for numeric ranges eg colors_h [0..5] = '#CD5C5C'
+  def initialize(stats_h = {}, colors_h = {})
+    @stats_h, @colors_h = stats_h, colors_h
+  end
 
-    # == Returns:
-    # A colorized SVG (Scalable Vector Graphics) map of the USA 
-    def to_s
-      css = '' 
-      @stats_h.each do |fips, val|
-        colr = @colors_h.select { |range, hex| range.member?(val) }.values.first  
-        css << ".c#{fips} {fill:#{colr}}\n"
-      end
-      @svg.gsub!(/\/\*CSS insertion point\*\//, css)
+  # == Returns:
+  # A colorized SVG (Scalable Vector Graphics) map of the USA 
+  def to_s
+    css = '' 
+    @stats_h.each do |fips, val|
+      colr = @colors_h.select { |range, hex| range.member?(val) }.values.first  
+      css << ".c#{fips} {fill:#{colr}}\n"
     end
+    UsaCountiesSvg.svg.gsub!(/\/\*CSS insertion point\*\//, css)
   end
 end
